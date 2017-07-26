6e4d6feb008e7bd09a3d6b9856817909345ff6e7
matew17 <matew-17@hotmail.com>
2017-07-26 14:17:02 -0500

    Fixing images

--- src/app/students/data-mock/students-data.ts
+++ src/app/students/data-mock/students-data.ts
@@ -4,19 +4,19 @@ export class StudentsDataService implements InMemoryDbService {
     createDb() {
         const students = [
             {
-                id: 1, fullname: 'Mateo Castaño', photo: '../assets/images/goku.png',
+                id: 1, fullname: 'Mateo Castaño', photo: './assets/images/goku.png',
                 grade1: 5, grade2: 4, grade3: 5, grade4: 5, grade5: 5, infoupdated: '2017/07/23'
             },
             {
-                id: 2, fullname: 'Jose Castaño', photo: '../assets/images/batman.png',
+                id: 2, fullname: 'Jose Castaño', photo: './assets/images/batman.png',
                 grade1: 4, grade2: 4, grade3: 5, grade4: 0, grade5: null, infoupdated: '2017/07/23'
             },
             {
-                id: 3, fullname: 'Juan Castaño', photo: '../assets/images/batman.png',
+                id: 3, fullname: 'Juan Castaño', photo: './assets/images/batman.png',
                 grade1: 1, grade2: 4, grade3: 5, grade4: 0, grade5: 5, infoupdated: '2017/07/23'
             },
             {
-                id: 4, fullname: 'Wilson Castaño', photo: '../assets/images/batman.png',
+                id: 4, fullname: 'Wilson Castaño', photo: './assets/images/batman.png',
                 grade1: 1, grade2: 4, grade3: 5, grade4: 0, grade5: null, infoupdated: '2017/07/23'
             },
             {
@@ -24,11 +24,11 @@ export class StudentsDataService implements InMemoryDbService {
                 grade1: 1, grade2: 4, grade3: 5, grade4: 0, grade5: null, infoupdated: '2017/07/23'
             },
             {
-                id: 6, fullname: 'Ricardo', photo: '../assets/images/batman.png',
+                id: 6, fullname: 'Ricardo', photo: './assets/images/batman.png',
                 grade1: 1, grade2: 4, grade3: 5, grade4: 0, grade5: null, infoupdated: '2017/07/23'
             },
             {
-                id: 7, fullname: 'Marcos Arenas', photo: '../assets/images/goku.png',
+                id: 7, fullname: 'Marcos Arenas', photo: './assets/images/goku.png',
                 grade1: 1, grade2: 4, grade3: 5, grade4: 0, grade5: 4, infoupdated: '2017/07/23'
             }
         ];
